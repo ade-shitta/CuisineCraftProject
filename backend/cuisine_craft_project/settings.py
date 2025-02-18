7449eb996fe713117cd0c02de7f0d50890123d0c
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2025-02-18 15:31:35 +0000

    development environment fully setup

--- backend/cuisine_craft_project/settings.py
+++ backend/cuisine_craft_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'ingredients.apps.IngredientsConfig',
+    'recipes.apps.RecipesConfig',
+    'recommendations.apps.RecommendationsConfig',
 ]
 
 MIDDLEWARE = [
