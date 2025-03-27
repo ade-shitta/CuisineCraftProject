8fb796abed9e4acc2ce5df588fdd8bd28e4cd97f
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2025-03-27 01:50:52 +0000

    tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// frontend/tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "cupcake"],
+  },
+}
