07cc455cb46f06b550e7ed002c568e0ad4dfac0e
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2025-04-14 22:40:39 +0100

    minor changes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import { useState, useRef, useEffect } from "react"
 import { useNavigate, useLocation } from "react-router-dom"
--- frontend/src/pages/RecipeDetailsPage.tsx
+++ frontend/src/pages/RecipeDetailsPage.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import { useEffect, useState, useCallback } from "react"
 import { useParams, useNavigate, useLocation } from "react-router-dom"
